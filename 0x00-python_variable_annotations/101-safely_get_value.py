59e32433b2ba65e1949e0dbc8c8a520210b7f3af
Kayr0r404 <mahlarekarabo.702@gmail.com>
2024-04-19 05:45:57 +0200

    safety

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -1,5 +1,14 @@
 #!/usr/bin/env python3
-def safely_get_value(dct, key, default=None):
+from typing import Any, Mapping, Union, TypeVar
+
+"""Task 11's module.
+"""
+
+
+def safely_get_value(
+    dct: Mapping, key: Any, default: Union[Any, TypeVar("T")] = None
+) -> Union[Any, TypeVar("T")]:
+    """Retrieves a value from a dict using a given key."""
     if key in dct:
         return dct[key]
     else:
